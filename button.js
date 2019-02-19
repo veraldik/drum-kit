5fa70556b6e31dd42cb489016ef8aa4efce9f0e8
Kimbo <Kimbo@Kimbos-MacBook-Pro.local>
2019-02-18 22:26:45 -0500

    drum kit

---
+++ button.js
@@ -0,0 +1,18 @@
+var buttonElement = document.getElementById("button1")
+buttonElement.addEventListener('click', function (){
+  var audio1 = document.getElementById("boom");
+  audio1.play();
+})
+
+var buttonElement = document.getElementById("button2")
+buttonElement.addEventListener('click', function (){
+  var audio2 = document.getElementById("clap");
+  audio2.play();
+})
+
+var buttonElement = document.getElementById("button3")
+buttonElement.addEventListener('click', function (){
+  var audio3 = document.getElementById("hihat");
+  audio3.play();
+})
+
